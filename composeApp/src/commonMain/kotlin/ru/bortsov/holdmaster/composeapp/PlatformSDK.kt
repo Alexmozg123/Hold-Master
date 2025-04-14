50ee3cc656f5d96979f800dc480236f5f8c7461a
Alex <alexwot1212@yandex.ru>
2025-04-14 17:49:49 +0300

    Added a method `getRootFactory` to `PlatformSDK` for iOS, without `lazy`, `inline` and `reified` modifiers.

--- composeApp/src/commonMain/kotlin/ru/bortsov/holdmaster/composeapp/PlatformSDK.kt
+++ composeApp/src/commonMain/kotlin/ru/bortsov/holdmaster/composeapp/PlatformSDK.kt
@@ -2,6 +2,9 @@ package ru.bortsov.holdmaster.composeapp
 
 import org.koin.dsl.koinApplication
 import org.koin.dsl.module
+import ru.bortsov.holdmaster.composeapp.decompose.Root
+import ru.bortsov.holdmaster.core.base.di.Inject
+import ru.bortsov.holdmaster.core.base.di.Inject.di
 import ru.bortsov.holdmaster.core.base.platform.PlatformConfig
 import ru.bortsov.holdmaster.feature.photo.data.photoDataModule
 import ru.bortsov.holdmaster.feature.photo.presentation.photoPresentationModule
@@ -22,4 +25,12 @@ object PlatformSDK {
             }.koin
         )
     }
+
+    /**
+     * ТОЛЬКО ДЛЯ IOS
+     *
+     * Важно: убираем lazy, inline и reified
+     * из-за особенностей компиляции Kotlin в Objective-C
+     */
+    fun getRootFactory(): Root.Factory { return di.get<Root.Factory>() }
 }
