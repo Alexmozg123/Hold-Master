63f335ec62d53b6c8f1cd01646abf4d28098dc0e
Alexmozg123 <alexwot1212@yandex.ru>
2025-04-25 10:46:42 +0300

    Refactor: Remove unnecessary dependency from photo API module
    - Removed `androidx.activity.ktx` dependency from `photo-api` module's `androidMain`.

--- feature/photo/api/build.gradle.kts
+++ feature/photo/api/build.gradle.kts
@@ -2,14 +2,6 @@ plugins {
     alias(libs.plugins.holdmaster.kmplib)
 }
 
-kotlin {
-    sourceSets {
-        androidMain.dependencies {
-            implementation(libs.androidx.activity.ktx)
-        }
-    }
-}
-
 android {
     namespace = "ru.bortsov.holdmaster.feature.photo.api"
 }
